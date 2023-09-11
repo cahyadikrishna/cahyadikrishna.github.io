eab8a6f4d21bd6bc9710ed961440088a5d5c9068
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-09-11 11:42:26 +0800

    Change contact link and logo icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useLottie } from "lottie-react";
-import comingSoonImg from "./assets/think-animation.json";
+import { useLottie } from 'lottie-react';
 import {
   AiOutlineInstagram,
   AiOutlineMail,
   AiOutlineWhatsApp,
-} from "react-icons/ai";
+} from 'react-icons/ai';
+
+import chyLogo from '../public/chy-favicon.svg';
+import comingSoonImg from './assets/think-animation.json';
 
 export default function App() {
   const heroImg = {
@@ -16,6 +18,11 @@ export default function App() {
   return (
     <section className="container mx-auto">
       <div className="flex flex-col h-screen justify-center">
+        <div className="flex gap-[12px] items-center justify-center">
+          <img src={chyLogo} alt="Cahyadi Logo" className="h-[32px]" />
+          <span className="font-bold text-[20px]">Cahyadi Krishna</span>
+        </div>
+
         <div className="flex justify-center -mt-[60px]">
           <div className="sm:w-[400px] sm:h-[400px] w-[300px] h-[300px] ">
             {View}
@@ -23,11 +30,9 @@ export default function App() {
         </div>
 
         <div className="text-center -mt-[50px]">
-          <h1 className=" sm:text-[62px] text-[38px] font-bold">
-            Coming Soon!
-          </h1>
+          <h1 className="sm:text-[62px] text-[38px] font-bold">Coming Soon!</h1>
           <p className="sm:text-[22px] text-[20px] mt-2">
-            Will be out ASAP :){" "}
+            Will be out ASAP :){' '}
             <sup className="text-[12px] text-gray-400">*if i want it</sup>
           </p>
         </div>
@@ -42,14 +47,14 @@ export default function App() {
           </a>
           <a
             className="hover:text-gray-400"
-            href="https://www.instagram.com/humanit__/"
+            href="https://www.instagram.com/cahyadikrishnaw"
             target="_blank"
           >
             <AiOutlineInstagram size={32} />
           </a>
           <a
             className="hover:text-gray-400"
-            href="https://wa.me/089606620616"
+            href="https://wa.me/+6289606620616"
             target="_blank"
           >
             <AiOutlineWhatsApp size={32} />
